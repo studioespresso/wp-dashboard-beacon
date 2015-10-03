2e1f3823270fb173ed2e878fbde9fea76cc381cc
Jan Henckens <jan@onedge.be>
2015-10-03 10:59:47 +0200

    Whitespace cleanup

--- admin/class-wp-dashboard-beacon-admin.php
+++ admin/class-wp-dashboard-beacon-admin.php
@@ -23,30 +23,30 @@
 class Wp_Dashboard_Beacon_Admin {
 
 	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
+    * The ID of this plugin.
+    *
+    * @since    1.0.0
+    * @access   private
+    * @var      string    $plugin_name    The ID of this plugin.
+    */
 	private $plugin_name;
 
 	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
+    * The version of this plugin.
+    *
+    * @since    1.0.0
+    * @access   private
+    * @var      string    $version    The current version of this plugin.
+    */
 	private $version;
 
 	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
+    * Initialize the class and set its properties.
+    *
+    * @since    1.0.0
+    * @param      string    $plugin_name       The name of this plugin.
+    * @param      string    $version    The version of this plugin.
+    */
 	public function __construct( $plugin_name, $version ) {
 
 		$this->plugin_name = $plugin_name;
@@ -55,23 +55,23 @@ class Wp_Dashboard_Beacon_Admin {
 	}
 
 	/**
-	 * Register the stylesheets for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
+    * Register the stylesheets for the admin area.
+    *
+    * @since    1.0.0
+    */
 	public function enqueue_styles() {
 
 		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wp_dashboard_Beacon_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wp_dashboard_Beacon_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        * This function is provided for demonstration purposes only.
+        *
+        * An instance of this class should be passed to the run() function
+        * defined in Wp_dashboard_Beacon_Loader as all of the hooks are defined
+        * in that particular class.
+        *
+        * The Wp_dashboard_Beacon_Loader will then create the relationship
+        * between the defined hooks and the functions defined in this
+        * class.
+        */
 
 
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-dashboard-beacon-admin.css', array(), $this->version, 'all' );
@@ -79,23 +79,23 @@ class Wp_Dashboard_Beacon_Admin {
 	}
 
 	/**
-	 * Register the JavaScript for the admin area.
-	 *
-	 * @since    1.0.0
-	 */
+    * Register the JavaScript for the admin area.
+    *
+    * @since    1.0.0
+    */
 	public function enqueue_scripts() {
 
 		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Wp_dashboard_Beacon_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Wp_dashboard_Beacon_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+        * This function is provided for demonstration purposes only.
+        *
+        * An instance of this class should be passed to the run() function
+        * defined in Wp_dashboard_Beacon_Loader as all of the hooks are defined
+        * in that particular class.
+        *
+        * The Wp_dashboard_Beacon_Loader will then create the relationship
+        * between the defined hooks and the functions defined in this
+        * class.
+        */
 
         wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-dashboard-beacon-admin.js', array( 'jquery' ), $this->version, false );
         wp_localize_script( $this->plugin_name, 'hsb_settings', array(
@@ -103,6 +103,9 @@ class Wp_Dashboard_Beacon_Admin {
             'subDomain' => get_option('hsb_helpscout_subdomain'),
             'beaconOptions' => get_option('hsb_beacon_options'),
             'icon' => get_option('hsb_beacon_icon'),
+            'credits' => get_option('hsb_show_credits'),
+            'formInstructions' => get_option('hsb_form_instructions'),
+            'allowAttachments' => get_option('hsb_allow_attachments'),
             'strings' => array(
                 'searchLabel' => __('What can we help you with?', 'wp-dashboard-beacon'),
                 'searchErrorLabel' => __('Your search timed out. Please double-check your internet connection and try again.', 'wp-dashboard-beacon'),
@@ -283,10 +286,5 @@ class Wp_Dashboard_Beacon_Admin {
 	public function hsb_add_settings_page() {
 		add_submenu_page( 'tools.php', 'Dashboard Beacon', 'Dashboard Beacon', 'manage_options', 'dashboard_beacon', array( $this, 'hsb_add_settings_page_callback') );
 	}
-
-
-
-
-
 
 }
