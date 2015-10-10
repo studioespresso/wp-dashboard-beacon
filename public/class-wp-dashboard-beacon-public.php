bb0b5570d3be94432f57f20fe545962ff36964c6
Jan Henckens <jan@onedge.be>
2015-10-10 20:15:39 +0200

    Disable public enqueues

--- public/class-wp-dashboard-beacon-public.php
+++ public/class-wp-dashboard-beacon-public.php
@@ -73,7 +73,7 @@ class Wp_dashboard_Beacon_Public {
 		 * class.
 		 */
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-dashboard-beacon-public.css', array(), $this->version, 'all' );
+		// wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/wp-dashboard-beacon-public.css', array(), $this->version, 'all' );
 
 	}
 
@@ -96,7 +96,7 @@ class Wp_dashboard_Beacon_Public {
 		 * class.
 		 */
 
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-dashboard-beacon-public.js', array( 'jquery' ), $this->version, false );
+		// wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/wp-dashboard-beacon-public.js', array( 'jquery' ), $this->version, false );
 
 	}
 
