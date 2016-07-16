1ff784371b070ecde8a86a970633fb0c648243c5
Jan Henckens <jan@studioespresso.co>
2016-07-16 12:18:53 +0200

    Added plugin name & construct

--- admin/class-wp-dashboard-beacon-multisite.php
+++ admin/class-wp-dashboard-beacon-multisite.php
@@ -23,6 +23,38 @@
 class Wp_dashboard_Beacon_Multisite {
 
         /**
+    * The ID of this plugin.
+    *
+    * @since    1.0.0
+    * @access   private
+    * @var      string    $plugin_name    The ID of this plugin.
+    */
+    private $plugin_name;
+
+    /**
+    * The version of this plugin.
+    *
+    * @since    1.0.0
+    * @access   private
+    * @var      string    $version    The current version of this plugin.
+    */
+    private $version;
+
+    /**
+    * Initialize the class and set its properties.
+    *
+    * @since    1.0.0
+    * @param      string    $plugin_name       The name of this plugin.
+    * @param      string    $version    The version of this plugin.
+    */
+    public function __construct( $plugin_name, $version ) {
+
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+
+    }
+
+    /**
     * Register the stylesheets for the admin area.
     *
     * @since    1.0.0
